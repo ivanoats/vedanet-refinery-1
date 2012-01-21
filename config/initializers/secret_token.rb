395e7275133163c6e2c95c22ed5f729fcaa4cf5a
Ivan Storck <ivanoats@gmail.com>
2012-01-21 10:10:57 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vedanet::Application.config.secret_token = '54e77da6ce15c06aea46d15805805e7a97ba3faa580f31662aa832ebccbe02f006eb445d24a89300909d3cdcbec3477121fcb9d38529f81246d86c34fbf54b06'
